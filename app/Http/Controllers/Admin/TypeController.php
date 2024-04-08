b1c374dbd83cd489436fafcf364490701ecd015d
DemetrioPadre <148373150+DemetrioPadre@users.noreply.github.com>
2024-04-08 21:12:23 +0200

    Initial commit

---
+++ app/Http/Controllers/Admin/TypeController.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Models\Type;
+use Illuminate\Http\Request;
+
+class TypeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $types = Type::orderBy('id', 'DESC')->paginate(10);
+        return view('admin.types.index', compact('types'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        $type = new Type;
+        return view('admin.types.form', compact('type'));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $data = $request->all();
+        $type = new Type;
+        $type->fill($data);
+
+        $type->save();
+
+        return redirect()->route('admin.types.show', $type);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Type  $type
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Type $type)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Type  $type
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Type $type)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Type  $type
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Type $type)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Type  $type
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Type $type)
+    {
+        //
+    }
+}
---
+++ app/Models/Project.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Project extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = ['type_id', 'title', 'content'];
+    public function type()
+    {
+        return $this->belongsTo(Type::class);
+    }
+
+
+    public function getAbstract($n_chars = 30)
+    {
+        return (strlen($this->content) > $n_chars) ? substr($this->content, 0, $n_chars) . '...' : $this->content;
+    }
+}
