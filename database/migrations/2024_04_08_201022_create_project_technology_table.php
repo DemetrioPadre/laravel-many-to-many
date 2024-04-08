a7d75275b7e924e388eef7b8adf8758d6d57cc16
DemetrioPadre <demetriopadre@gmail.com>
2024-04-08 22:46:57 +0200

    add seeder technology

--- database/migrations/2024_04_08_201022_create_project_technology_table.php
+++ database/migrations/2024_04_08_201022_create_project_technology_table.php
@@ -20,7 +20,7 @@ return new class extends Migration
 
             $table->foreignId('technology_id')->constrained()->cascadeOnDelete();
 
-            $table->timestamps();
+            // $table->timestamps();
         });
     }
 
